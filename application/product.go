1f69bdb0dbc7b4c9b40261d077e030fb5d40d047
Lucas Rego <lucas.sr.rg@gmail.com>
2022-12-10 08:13:25 -0300

    feature/define product interface

---
+++ application/product.go
@@ -0,0 +1,51 @@
+package application
+
+type ProductInterface interface {
+	IsValid() (bool, error)
+	Enable() error
+	Disable() error
+	GetID() string
+	GetName() string
+	GetStatus() string
+	GetPrice() float64
+}
+
+const (
+	DISABLED = "disabled"
+	ENABLED  = "enabled"
+)
+
+type Product struct {
+	ID     string  `json:"id"`
+	Name   string  `json:"name"`
+	Status string  `json:"status"`
+	Price  float64 `json:"price"`
+}
+
+func (p *Product) IsValid() (bool, error) {
+
+}
+
+func (p *Product) Enable() error {
+
+}
+
+func (p *Product) Disable() error {
+
+}
+
+func (p *Product) GetID() string {
+	return p.ID
+}
+
+func (p *Product) GetName() string {
+	return p.Name
+}
+
+func (p *Product) GetStatus() string {
+	return p.Status
+}
+
+func (p *Product) GetPrice() float64 {
+	return p.Price
+}
